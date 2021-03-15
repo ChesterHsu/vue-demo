1bb221d5d0856ed441d3c9873ef005f8c66f93cd
徐嘉輝 <aa0909286667@gmail.com>
2021-03-15 18:01:06 +0800

    評論區版型

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,22 @@
         <button>Python 3</button>
       </div>
     </div>
+    <div class="evaluation">
+      <h3>口碑排行</h3>
+      <div class="comment">
+        <div class="comment-item" v-for="i in 4">
+          <img src="@/assets/images/avatar1.png">
+          <p>小強</p>
+          <p class="date">2021/03/07</p>
+          <h3><b>內容很詳細很充分很棒的課程，可以學到很多東西，整體節奏較快，有些變數或函式的用法會希望老師可以順帶提一下，幫助基礎不好的同學減少學習上的阻力。</b></h3>
+          <div class="empty-stars"></div>
+          <div class="course-item">
+            <img src="@/assets/images/course.png">
+            <p>Android UI 進階實戰 - 打造App絕佳體驗</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
