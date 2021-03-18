066c630fa790b0b3b83540a2c5b021801fdc1aaa
徐嘉輝 <aa0909286667@gmail.com>
2021-03-18 13:39:52 +0800

    introduction 基本版型配置

--- components/core/Header.vue
+++ components/core/Header.vue
@@ -2,22 +2,22 @@
   <header class="header">
     <div class="wrap">
       <div class="logo float-left">
-        <a href="#">
+        <a href="#" type="button">
           <img src="@/assets/images/logo.png">
         </a>
       </div>
       <div class="search float-left">
         <form class="float-left">
           <input class="search-button float-left" type="submit" value="">
-          <input class="text float-left" type="text" value="搜尋">
+          <input class="text float-left" type="text" placeholder="搜尋">
         </form>
       </div>
       <button class="registered float-right" type="button" >註冊</button>
       <button class="login-btn float-right" type="button" >登入</button>
-      <input class="question-circle-btn float-right" value="" type="button">
-      <input class="shopping-car-btn float-right" value="" type="button">
+      <a href="https://help.hiskio.com/zh-TW/" class="question-circle-btn float-right" type="button"></a>
+      <a href="#" class="shopping-car-btn float-right" type="button"></a>
       <div class="teach float-right">
-        <a href="#">我想開課</a>
+        <a href="#" type="button">我想開課</a>
       </div>
     </div>
   </header>
--- pages/index.vue
+++ pages/index.vue
@@ -32,10 +32,27 @@
         <button>Python 3</button>
       </div>
     </div>
+    <div class="introduction">
+      <img src="@/assets/images/introduction-1.png">
+      <div class="content">
+        <h3>專業</h3>
+        <p>堅持教學回歸專業，嚴選專業師資、定期篩選</p>
+      </div>
+      <img src="@/assets/images/introduction-2.png">
+      <div class="content">
+        <h3>深入</h3>
+        <p>不止步於入門，提供更深入的中高階、應用、實戰內容</p>
+      </div>
+      <img src="@/assets/images/introduction-3.png">
+      <div class="content">
+        <h3>實用</h3>
+        <p>傳授工作用得到的技巧，學習實務必備的技術</p>
+      </div>
+    </div>
     <div class="course">
       <div class="title">
         <h3>正在募資中的課程</h3>
-        <a href="#">更多募資課程</a>
+        <a href="https://hiskio.com/search/explore?professions=0&type=attendance&level=ALL&coursesType=courses&status=PRESALE" type="button">更多募資課程</a>
       </div>
       <div class="contents" v-for="i in 8">
         <div class="course-item">
